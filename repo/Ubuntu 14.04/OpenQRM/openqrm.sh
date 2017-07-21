5e802896183a340ea47deca94d9561226ae8f4b0
Angga Saputra <duaperak.id@gmail.com>
2017-07-22 03:00:39 +0700

    OpenQRM Cloud Computing Platform on Ubuntu 14.04
    System Requirements:
    
    Make sure your server meets below requirements.
    
    Minimum:
    
    virtual or physical system
    1 GB RAM
    20+ GB free hard disk space
    1 network interface card (1 Gbit/s)
    
    Recommended:
    
    Physical system
    4 GB or more RAM
    100+ GB free hard disk space
    1 network interface card (1 Gbit/s)
    
    Simple POC setup:
    
    If you are planning to evaluate openQRM, your system should atleast meet below requirement.
    
    Physical system
    VT CPU extension (full virtualization support) activated
    1 GB RAM
    20GB HDD
    1 network interface card (1 Gbit/s)

---
+++ repo/Ubuntu 14.04/OpenQRM/openqrm.sh
@@ -0,0 +1,6 @@
+sudo apt-get update
+wget http://downloads.sourceforge.net/project/openqrm/openQRM-Community-5.2/openQRM-5.2.1.1-Community-Edition.tgz
+sudo tar -zxvf openQRM-5.2.1.1-Community-Edition.tgz
+cd
+openQRM-5.2.1.1-Community-Edition/
+sudo ./install-openqrm.sh
